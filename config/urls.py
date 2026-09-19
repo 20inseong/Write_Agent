fa2d71d6f70f61689ad983432597c2454ac48b53
inseong20 <jeong3647in@gmail.com>
2026-09-19 10:05:43 +0900

    switch the admin adress

--- config/urls.py
+++ config/urls.py
@@ -5,7 +5,7 @@ from writing_agent import views as agent_views
 from writing_agent.forms import CustomLoginForm
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("adminw-7x9q2p/", admin.site.urls),
     path("", include("writing_agent.urls")),
     path('signup/', agent_views.signup_view, name='signup'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html', authentication_form=CustomLoginForm), name='login'),
